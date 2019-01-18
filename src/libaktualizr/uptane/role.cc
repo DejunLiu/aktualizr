9342b3e6812c1a5f84115a19f900d424e6750420
Patrick Vacek <patrickvacek@gmail.com>
2019-01-18 11:41:01 +0100

    Better role error checking with tests.
    Signed-off-by: Patrick Vacek <patrickvacek@gmail.com>

--- src/libaktualizr/uptane/role.cc
+++ src/libaktualizr/uptane/role.cc
@@ -5,20 +5,29 @@
 using Uptane::Role;
 using Uptane::Version;
 
+const std::string UPTANE_ROLE_ROOT = "root";
+const std::string UPTANE_ROLE_SNAPSHOT = "snapshot";
+const std::string UPTANE_ROLE_TARGETS = "targets";
+const std::string UPTANE_ROLE_TIMESTAMP = "timestamp";
+
 Role::Role(const std::string &role_name, const bool delegation) {
   std::string role_name_lower;
   std::transform(role_name.begin(), role_name.end(), std::back_inserter(role_name_lower), ::tolower);
-  if (role_name_lower == "root") {
-    role_ = RoleEnum::kRoot;
-  } else if (role_name_lower == "snapshot") {
-    role_ = RoleEnum::kSnapshot;
-  } else if (role_name_lower == "targets") {
-    role_ = RoleEnum::kTargets;
-  } else if (role_name_lower == "timestamp") {
-    role_ = RoleEnum::kTimestamp;
-  } else if (delegation) {
+  if (delegation) {
+    if (role_name_lower == UPTANE_ROLE_ROOT || role_name_lower == UPTANE_ROLE_SNAPSHOT ||
+        role_name_lower == UPTANE_ROLE_TARGETS || role_name_lower == UPTANE_ROLE_TIMESTAMP) {
+      throw Uptane::Exception("", "Invalid delegated role name " + role_name);
+    }
     role_ = RoleEnum::kDelegated;
     name_ = role_name;
+  } else if (role_name_lower == UPTANE_ROLE_ROOT) {
+    role_ = RoleEnum::kRoot;
+  } else if (role_name_lower == UPTANE_ROLE_SNAPSHOT) {
+    role_ = RoleEnum::kSnapshot;
+  } else if (role_name_lower == UPTANE_ROLE_TARGETS) {
+    role_ = RoleEnum::kTargets;
+  } else if (role_name_lower == UPTANE_ROLE_TIMESTAMP) {
+    role_ = RoleEnum::kTimestamp;
   } else {
     role_ = RoleEnum::kInvalidRole;
   }
@@ -27,13 +36,13 @@ Role::Role(const std::string &role_name, const bool delegation) {
 std::string Role::ToString() const {
   switch (role_) {
     case RoleEnum::kRoot:
-      return "root";
+      return UPTANE_ROLE_ROOT;
     case RoleEnum::kSnapshot:
-      return "snapshot";
+      return UPTANE_ROLE_SNAPSHOT;
     case RoleEnum::kTargets:
-      return "targets";
+      return UPTANE_ROLE_TARGETS;
     case RoleEnum::kTimestamp:
-      return "timestamp";
+      return UPTANE_ROLE_TIMESTAMP;
     case RoleEnum::kDelegated:
       return name_;
     default:
